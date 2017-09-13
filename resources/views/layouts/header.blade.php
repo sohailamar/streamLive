1840a47e3cf549fd0d2ba42dc6435c49c90029ce
sohail amar <sohailamar09@gmail.com>
2017-09-13 17:40:32 +0500

    updated

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -8,12 +8,12 @@
     <!----start-top-nav---->
     <div class="top-nav">
         <ul>
-            <li><a href="index.html">Home</a><p>My Forntpage</p></li>
+            <li><a href="{{url('/')}}">Home</a><p>My Forntpage</p></li>
             <li><a href="{{url('/channels')}}">Channels</a><p></p></li>
             <li><a href="categories.html">Categories</a><p>Be Ur Self</p></li>
             <li><a href="#">Economics</a><p>Human Needs</p></li>
             <li><a href="#">Health</a><p>Take A Trip</p></li>
-            <li><a href="contact.html">Contact</a><p>Leave Messages</p></li>
+            <li><a href="#">Contact</a><p>Leave Messages</p></li>
         </ul>
     </div>
     <div class="clear"> </div>
